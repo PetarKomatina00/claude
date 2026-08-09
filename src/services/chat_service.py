8dc20bed603388291148936425645f3e4df17b26
Petar Komatina <petarkkomatina00@gmail.com>
2026-08-09 18:44:05 +0200

    Added system prompting. Also added that we can dynamically change whenether or not to use a different system promtping

--- src/services/chat_service.py
+++ src/services/chat_service.py
@@ -5,6 +5,11 @@ from config import ANTHROPIC_MODEL
 class ChatService:
     def __init__(self):
         self.message_history = []
+        self.system = """
+            You are an AI Camping and Weather Assistant integrated into a WeatherDashboard application.
+            Your primary goal is to help users decide whether current or forecast weather conditions are suitable for camping, hiking, and other outdoor activities.
+            You will receive weather data from the application. Base your recommendations primarily on this provided data.
+        """
 
     def add_message(self, role: str, text: str) -> None:
         self.message_history.append({
@@ -12,14 +17,19 @@ class ChatService:
             "content": text,
         })
 
-    def ask(self, question: str) -> str:
+    def ask(self, question: str, system=None) -> str:
+
+        params = {
+            "model" : ANTHROPIC_MODEL,
+            "max_tokens" : 1024,
+            "messages" : self.message_history,
+            "system" : self.system
+        }
+        if system:
+            params["system"] = system
         self.add_message("user", question)
 
-        response = client.messages.create(
-            model=ANTHROPIC_MODEL,
-            max_tokens=1024,
-            messages=self.message_history,
-        )
+        response = client.messages.create(**params)
 
         answer = response.content[0].text
 
